edfd295cd91c78dffb40a3c5dc4e1a95d870fa1f
Marvin Blum <marvin.blum@emvi.com>
2024-07-16 15:55:02 +0200

    Dynamic filter configuration.

--- cli/main.go
+++ cli/main.go
@@ -16,14 +16,13 @@ type column struct {
 }
 
 type config struct {
-	ClientID     string   `json:"client_id"`
-	ClientSecret string   `json:"client_secret"`
-	StartDate    string   `json:"start_date"`
-	EndDate      string   `json:"end_date"`
-	EventMetaKey string   `json:"event_meta_key"`
-	EventPrinted string   `json:"event_printed"`
-	EventClicked string   `json:"event_clicked"`
-	Columns      []column `json:"columns"`
+	ClientID     string        `json:"client_id"`
+	ClientSecret string        `json:"client_secret"`
+	Filter       pirsch.Filter `json:"filter"`
+	EventMetaKey string        `json:"event_meta_key"`
+	EventPrinted string        `json:"event_printed"`
+	EventClicked string        `json:"event_clicked"`
+	Columns      []column      `json:"columns"`
 }
 
 func main() {
@@ -55,57 +54,40 @@ func main() {
 		return
 	}
 
-	start, err := time.Parse(time.DateOnly, cfg.StartDate)
-
-	if err != nil {
-		log.Println("Error parsing start date: ", err)
-		return
-	}
-
-	end, err := time.Parse(time.DateOnly, cfg.EndDate)
-
-	if err != nil {
-		log.Println("Error parsing end date: ", err)
-		return
-	}
-
 	var buffer bytes.Buffer
 	buffer.WriteString("PeriodStart,PeriodEnd,CampaignName,Device,AdvertPrinted,AdvertClicked\n")
+	cfg.Filter.DomainID = domain.ID
 
 	for _, column := range cfg.Columns {
-		statsPrinted, err := client.TotalVisitors(&pirsch.Filter{
-			DomainID: domain.ID,
-			From:     start,
-			To:       end,
-			Event:    []string{cfg.EventPrinted},
-			EventMeta: map[string]string{
-				cfg.EventMetaKey: column.Campaign,
-			},
-			OS: []string{"null"},
-		})
+		cfg.Filter.Event = []string{cfg.EventPrinted}
+		cfg.Filter.EventMetaKey = []string{cfg.EventMetaKey}
+		cfg.Filter.EventMeta = map[string]string{
+			cfg.EventMetaKey: column.Campaign,
+		}
+		statsPrinted, err := client.EventMetadata(&cfg.Filter)
 
 		if err != nil {
 			log.Println("Error getting visitors: ", err)
 			return
 		}
 
-		statsClicked, err := client.TotalVisitors(&pirsch.Filter{
-			DomainID: domain.ID,
-			From:     start,
-			To:       end,
-			Event:    []string{cfg.EventClicked},
-			EventMeta: map[string]string{
-				cfg.EventMetaKey: column.Campaign,
-			},
-			OS: []string{"null"},
-		})
+		if len(statsPrinted) == 0 {
+			continue
+		}
+
+		cfg.Filter.Event = []string{cfg.EventClicked}
+		statsClicked, err := client.EventMetadata(&cfg.Filter)
 
 		if err != nil {
 			log.Println("Error getting visitors: ", err)
 			return
 		}
 
-		buffer.WriteString(fmt.Sprintf("%s,%s,%s,%s,%d,%d\n", start.Format(time.DateOnly), end.Format(time.DateOnly), column.Campaign, column.Device, statsPrinted.Visitors, statsClicked.Visitors))
+		if len(statsClicked) == 0 {
+			continue
+		}
+
+		buffer.WriteString(fmt.Sprintf("%s,%s,%s,%s,%d,%d\n", cfg.Filter.From.Format(time.DateOnly), cfg.Filter.To.Format(time.DateOnly), column.Campaign, column.Device, statsPrinted[0].Visitors, statsClicked[0].Visitors))
 	}
 
 	if err := os.WriteFile("report.csv", buffer.Bytes(), 0644); err != nil {
